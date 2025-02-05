8498e059f943bdb14182a93e1782500439ab42f4
mkayac45 <mkayac45@gmail.com>
2025-02-05 15:20:11 +0300

    Entities configuration added

---
+++ BookManager.Persistence/Authors/AuthorConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BookManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManager.Persistence.Authors
+{
+    public class AuthorConfiguration:IEntityTypeConfiguration<Author>
+    {
+
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(c => c.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(c => c.Genre).IsRequired().HasMaxLength(50);
+        }
+
+    }
+}
