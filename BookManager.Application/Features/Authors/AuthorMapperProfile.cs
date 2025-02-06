613fdb0421a8053dd94d1404ff67c9c1a68f7e23
mkayac45 <mkayac45@gmail.com>
2025-02-06 11:25:59 +0300

    Added author service

---
+++ BookManager.Application/Features/Authors/AuthorMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookManager.Application.Features.Authors.Dto;
+using BookManager.Domain.Entities;
+
+namespace BookManager.Application.Features.Authors
+{
+    public class AuthorMapperProfile:Profile
+    {
+        public AuthorMapperProfile()
+        {
+            CreateMap<Author,AuthorDto>().ReverseMap();
+        }
+    }
+}
---
+++ BookManager.Application/Features/Authors/IAuthorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookManager.Application.Features.Authors.Create;
+using BookManager.Application.Features.Authors.Dto;
+
+namespace BookManager.Application.Features.Authors
+{
+    public interface IAuthorService
+    {
+        Task<ServiceResult<List<AuthorDto>>> GetAllAsync();
+        Task<ServiceResult<AuthorDto>> GetByIdAsync(Guid id);
+        Task<ServiceResult<CreateAuthorResponse>> CreateAsync(CreateAuthorRequest author);
+        Task<ServiceResult> UpdateAsync(AuthorDto author);
+        Task<ServiceResult> DeleteAsync(Guid id);
+
+
+
+    }
+}
